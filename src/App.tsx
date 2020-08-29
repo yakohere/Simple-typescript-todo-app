8cf7bcf7f47a655837f2816b6993921749c45d6e
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-08-29 23:23:25 +0500

    create a new to do

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+import NewTodo from "./components/NewTodo";
 import ToDoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course!" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
       <ToDoList items={todos} />
     </div>
   );
